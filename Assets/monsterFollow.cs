63c34af8b4906bb31a72eae517659e7b0a415d9b
k0nsun <johnatan.georges@viacesi.fr>
2015-11-14 09:33:32 +0100

    Scene Johnatan V1

---
+++ Assets/monsterFollow.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+
+public class monsterFollow : MonoBehaviour
+{
+    public GameObject player;
+    public Transform[] path;
+    public FSMSystem fsm;
+
+    public void SetTransition(Transition t) { fsm.PerformTransition(t); }
+
+    public void Start()
+    {
+        MakeFSM();
+    }
+
+    public void FixedUpdate()
+    {
+        fsm.CurrentState.Reason(player, gameObject);
+        fsm.CurrentState.Act(player, gameObject);
+    }
+
+    // The NPC has two states: FollowPath and ChasePlayer
+    // If it's on the first state and SawPlayer transition is fired, it changes to ChasePlayer
+    // If it's on ChasePlayerState and LostPlayer transition is fired, it returns to FollowPath
+    private void MakeFSM()
+    {
+        FollowPathState follow = new FollowPathState(path);
+        follow.AddTransition(Transition.SawPlayer, StateID.ChasingPlayer);
+
+        ChasePlayerState chase = new ChasePlayerState();
+        chase.AddTransition(Transition.LostPlayer, StateID.FollowingPath);
+
+        fsm = new FSMSystem();
+        fsm.AddState(follow);
+        fsm.AddState(chase);
+    }
+
+    //void OnTriggerEnter(Collider col)
+    //{
+    //    if (col.tag == "Player")
+    //    {
+    //        RaycastHit hit;
+    //        if (Physics.Linecast(transform.position, col.transform.position, out hit))
+    //        {
+    //            if (hit.collider.tag == "Player")
+    //            {
+    //                GetComponent<monsterFollow>().SetTransition(Transition.SawPlayer);
+    //            }
+    //        }
+    //    }
+    //}
+}
+
+public class FollowPathState : FSMState
+{
+    private int currentWayPoint;
+    private Transform[] waypoints;
+
+    public FollowPathState(Transform[] wp)
+    {
+        waypoints = wp;
+        currentWayPoint = 0;
+        stateID = StateID.FollowingPath;
+    }
+
+
+
+    public override void Reason(GameObject player, GameObject npc) {
+    }
+
+    public override void Act(GameObject player, GameObject npc)
+    {
+        // Follow the path of waypoints
+        // Find the direction of the current way point 
+        Vector3 vel = npc.GetComponent<Rigidbody>().velocity;
+        Vector3 moveDir = waypoints[currentWayPoint].position - npc.transform.position;
+
+        if (moveDir.magnitude < 1)
+        {
+            currentWayPoint++;
+            if (currentWayPoint >= waypoints.Length)
+            {
+                currentWayPoint = 0;
+            }
+        }
+        else
+        {
+            vel = moveDir.normalized * 10;
+
+            // Rotate towards the waypoint
+            npc.transform.rotation = Quaternion.Slerp(npc.transform.rotation,
+                                                      Quaternion.LookRotation(moveDir),
+                                                      5 * Time.deltaTime);
+            npc.transform.eulerAngles = new Vector3(0, npc.transform.eulerAngles.y, 0);
+
+        }
+
+        // Apply the Velocity
+        npc.GetComponent<Rigidbody>().velocity = vel;
+    }
+
+} // FollowPathState
+
+public class ChasePlayerState : FSMState
+{
+    public ChasePlayerState()
+    {
+        stateID = StateID.ChasingPlayer;
+    }
+
+    public override void Reason(GameObject player, GameObject npc)
+    {
+        // If the player has gone 30 meters away from the NPC, fire LostPlayer transition
+        if (Vector3.Distance(npc.transform.position, player.transform.position) >= 30)
+        {
+            npc.GetComponent<monsterFollow>().SetTransition(Transition.LostPlayer);
+        }
+        else
+        {
+            RaycastHit hit;
+            if (Physics.Linecast(npc.transform.position, player.transform.position, out hit))
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    npc.GetComponent<monsterFollow>().SetTransition(Transition.SawPlayer);
+                }
+            }
+        }
+
+
+
+    }
+
+    public override void Act(GameObject player, GameObject npc)
+    {
+        // Follow the path of waypoints
+        // Find the direction of the player 		
+        Vector3 vel = npc.GetComponent<Rigidbody>().velocity;
+        Vector3 moveDir = player.transform.position - npc.transform.position;
+
+        // Rotate towards the waypoint
+        npc.transform.rotation = Quaternion.Slerp(npc.transform.rotation,
+                                                  Quaternion.LookRotation(moveDir),
+                                                  5 * Time.deltaTime);
+        npc.transform.eulerAngles = new Vector3(0, npc.transform.eulerAngles.y, 0);
+
+        vel = moveDir.normalized * 10;
+
+        // Apply the new Velocity
+        npc.GetComponent<Rigidbody>().velocity = vel;
+    }
+
+} // ChasePlayerState
+
+
---
+++ Assets/testPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class testPlayer : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Enemy")
+        {
+            Debug.Log("touché");
+        }
+    }
+}
