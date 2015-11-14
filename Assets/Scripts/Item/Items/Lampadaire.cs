0523176c9f401b7aa999970446d63071d5e3a822
Baptiste Velado <Baptiste Velado>
2015-11-14 07:11:52 +0100

    Nouveau système de spawn des items + Torche + Lampadaire

---
+++ Assets/Scripts/Item/Items/Lampadaire.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Lampadaire : MonoBehaviour {
+    public float radius;
+
+    
+    void Update()
+    {
+        GetComponent<CapsuleCollider>().radius = radius;
+        GetComponent<Light>().range = radius;
+    }
+
+}
---
+++ Assets/Scripts/Item/Items/Torche.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torche : Item {
+
+    public float decayInSeconds = 70.0f;
+    float maxIntensity;
+
+    void Awake()
+    {
+        maxIntensity = GetComponent<Light>().intensity;
+    }
+
+    public override void handleInput()
+    {
+        base.handleInput();
+
+        // Bouchon pour test
+        // 
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StartCoroutine(Decay(decayInSeconds));
+        }
+    }
+
+    IEnumerator Decay(float duringSeconds)
+    {
+        Light light = GetComponent<Light>();
+        for(int i = (int)duringSeconds; i > -1; i--)
+        {
+            light.intensity = (i / duringSeconds) * maxIntensity;
+            yield return new WaitForSeconds(1.0f);
+        }
+        gameObject.SetActive(false);
+    }
+
+    void OnEnable()
+    {
+        GetComponent<Light>().intensity = maxIntensity;
+    }
+}
