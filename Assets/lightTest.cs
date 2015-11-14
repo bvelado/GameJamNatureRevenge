8b1b829d1679194c213a06190808e0eea4f878e0
k0nsun <johnatan.georges@viacesi.fr>
2015-11-15 00:51:12 +0100

    Music Light

---
+++ Assets/lightTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightTest : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            if (GameObject.FindGameObjectWithTag("GameController").GetComponent<AudioSource>().isPlaying)
+            {
+                GameObject.FindGameObjectWithTag("Musique2").GetComponent<AudioSource>().Play();
+                GameObject.FindGameObjectWithTag("GameController").GetComponent<AudioSource>().Stop();
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            if (GameObject.FindGameObjectWithTag("Musique2").GetComponent<AudioSource>().isPlaying)
+            {
+                GameObject.FindGameObjectWithTag("GameController").GetComponent<AudioSource>().Play();
+                GameObject.FindGameObjectWithTag("Musique2").GetComponent<AudioSource>().Stop();
+            }
+        }
+    }
+}
