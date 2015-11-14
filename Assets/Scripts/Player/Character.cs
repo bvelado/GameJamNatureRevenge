57444a8e6520f192a63201490cb29684e059133e
Cocooptique <corentin.mercier@insitutoptique>
2015-11-14 09:43:44 +0100

    Deplacement et rotation personnage

--- Assets/Scripts/Player/Character.cs
+++ Assets/Scripts/Player/Character.cs
@@ -5,6 +5,9 @@ public class Character : MonoBehaviour {
 	
 	private CharacterController controller;
 	private Vector3 moveDirection;
+	private Vector3 lastMoveDirection;
+	private float horizMove;
+	private float vertMove;
 	
 	// Use this for initialization
 	void Start ()
@@ -15,59 +18,18 @@ public class Character : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		//Droite
-		if(Input.GetKey(KeyCode.RightArrow))
-		{
-			moveDirection = new Vector3(0.5f,0,0);
-			//moveDirection *= speed;
-			
-		}
-		//Gauche
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			moveDirection = new Vector3(-0.5f, 0, 0);
-		}
-		//Haut
-		if (Input.GetKey(KeyCode.UpArrow))
-		{
-			moveDirection = new Vector3(0, 0, 0.5f);
-		}
-		//Bas
-		if (Input.GetKey(KeyCode.DownArrow))
-		{
-			moveDirection = new Vector3(0, 0, -0.5f);
-		}
-		//HautDroit
-		if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow))
-		{
-			moveDirection = new Vector3(0.5f, 0, 0.5f);
-		}
+		horizMove=Input.GetAxis ("Horizontal");
+		vertMove=Input.GetAxis ("Vertical");
+		moveDirection = new Vector3(horizMove*0.5f,0,vertMove*0.5f);
+
 		
-		//HautGauche
-		if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow))
-		{
-			moveDirection = new Vector3(-0.5f, 0, 0.5f);
+		if (moveDirection != Vector3.zero) {
+			lastMoveDirection = moveDirection;
 		}
-		
-		//BasDroit
-		if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.DownArrow))
-		{
-			moveDirection = new Vector3(0.5f, 0, -0.5f);
-		}
-		
-		//BasGauche
-		if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.DownArrow))
-		{
-			moveDirection = new Vector3(-0.5f, 0, -0.5f);
-		}
-		
-		//Pas bougé
-		if(!Input.anyKey)
-		{
-			moveDirection = new Vector3(0, 0, 0);
-		}
+
 		moveDirection.y -= 0.2f * 3;
 		controller.Move(moveDirection * 0.3f);
+		transform.FindChild("character").rotation = Quaternion.LookRotation(lastMoveDirection);
 		
 	}
 }
