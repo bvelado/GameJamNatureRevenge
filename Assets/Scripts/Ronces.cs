9301f0f5d6bb3217bf3bc4d9a0a8ecf151bd3ecc
Cocooptique <corentin.mercier@insitutoptique>
2015-11-15 16:21:39 +0100

    Build Level 1 et 2 qui fonctionne!!!!

---
+++ Assets/Scripts/Ronces.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ronces : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.transform.FindChild("FireParticles").gameObject.SetActive(false);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
