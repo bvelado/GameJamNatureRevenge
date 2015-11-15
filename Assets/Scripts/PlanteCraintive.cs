3281c5efeec461666fa8a96f53155bde6e4385c7
Cocooptique <corentin.mercier@insitutoptique>
2015-11-15 15:46:48 +0100

    truc

---
+++ Assets/Scripts/PlanteCraintive.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanteCraintive : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log ("entré");
+		if (col.tag == "BocalLucioles") {
+			Debug.Log ("Bocal");
+			Destroy(gameObject);
+		}
+	}
+
+}
