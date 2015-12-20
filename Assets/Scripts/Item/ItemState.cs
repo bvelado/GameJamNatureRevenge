e6e6ec5062d560a8542c88080774eeb1499c28da
thebatoust <thebatoust@gmail.com>
2015-12-20 17:10:52 +0100

    Commit Prefabs/V2 + scene V2 niveau 1

---
+++ Assets/Scripts/Item/ItemState.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemState {
+
+    protected ItemV2 _item;
+
+    // Initialisation de la state de l'objet
+    public virtual void Init(ItemV2 item)
+    {
+        _item = item;
+    }
+
+    public virtual void Update()
+    {
+
+    }
+
+    // Cleaning des variables de la state de l'objet
+    public virtual void Exit()
+    {
+
+    }
+
+    public virtual void Use()
+    {
+
+    }
+
+    public void ChangeState(ItemState nextState)
+    {
+        _item._state.Exit();
+        _item._state = nextState;
+        _item._state.Init(_item);
+    }
+}
---
+++ Assets/Scripts/Item/ItemV2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class ItemV2 : MonoBehaviour, IInventoryAttachable {
+
+    public ItemState _state;
+
+    public GameObject _model, _body;
+
+    public virtual void Awake() {
+        
+    }
+
+    public virtual void Start () {
+        
+    }
+
+    public void Update () {
+        _state.Update();
+	}
+
+    public void Use()
+    {
+        _state.Use();
+    }
+
+    public virtual void OnTriggerEnter(Collider col)
+    {
+        
+    }
+
+    public void Attach(Inventory inventory)
+    {
+        inventory.AddItem(this);
+    }
+
+    public void Detach()
+    {
+        GameObject.FindWithTag("Player").GetComponent<Inventory>().RemoveItem(this);
+    }
+
+}
